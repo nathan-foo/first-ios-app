588d4d644cd52ef713b3e7e673a8a17400c2d517
Nathan Foo <ntfoo27@outlook.com>
2024-10-17 19:17:25 -0500

    Initial Commit

---
+++ learn-ios/learn_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_iosApp.swift
+//  learn-ios
+//
+//  Created by Nathan Foo on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct learn_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ learn-iosTests/learn_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  learn_iosTests.swift
+//  learn-iosTests
+//
+//  Created by Nathan Foo on 10/17/24.
+//
+
+import Testing
+@testable import learn_ios
+
+struct learn_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
